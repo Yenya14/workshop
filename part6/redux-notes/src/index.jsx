c601bebcb5110c942f3692eb5136c5e8e52884a9
Yenya14 <leeza.mhr@gmail.com>
2023-09-11 02:01:07 +0545

    6 a complete

--- part6/redux-notes/src/index.jsx
+++ part6/redux-notes/src/index.jsx
@@ -1,45 +1,16 @@
 import { createRoot } from "react-dom/client";
-import {createStore} from "redux";
+import { createStore } from "redux";
+import App from "./App";
 import noteReducer from "./reducers/noteReducer";
-  
-  const store = createStore(noteReducer)
-  
-  store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-      content: 'the app state is in redux store',
-      important: true,
-      id: 1
-    }
-  })
-  
-  store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-      content: 'state changes are made with actions',
-      important: false,
-      id: 2
-    }
-  })
-  
-  const App = () => {
-    return(
-      <div>
-        <ul>
-          {store.getState().map(note=>
-            <li key={note.id}>
-              {note.content} <strong>{note.important ? 'important' : ''}</strong>
-            </li>
-          )}
-          </ul>
-      </div>
-    )
-  }
+import { Provider } from "react-redux";
+
+const store = createStore(noteReducer);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App/>);
-store.subscribe(()=>(
-    root.render(<App/>)
-))
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- part6/redux-notes/src/reducers/noteReducer.js
+++ part6/redux-notes/src/reducers/noteReducer.js
@@ -1,21 +1,38 @@
 const noteReducer = (state = [], action) => {
-    switch(action.type) {
-      case 'NEW_NOTE':
-        return state.concat(action.payload)
-      case 'TOGGLE_IMPORTANCE': {
-        const id = action.payload.id
-        const noteToChange = state.find(n => n.id === id)
-        const changedNote = { 
-          ...noteToChange, 
-          important: !noteToChange.important 
-        }
-        return state.map(note =>
-          note.id !== id ? note : changedNote 
-        )
-       }
-      default:
-        return state
+  console.log("action is", action);
+  console.log("state is", state);
+  switch (action.type) {
+    case "NEW_NOTE": {
+      const newState = state.concat(action.payload);
+      return newState;
     }
+    case "TOGGLE_IMPORTANCE": {
+      let myNote = state.find((note) => note.id === action.payload);
+      let changedNote = { ...myNote, important: !myNote.important };
+      //   changedNote.important = !changedNote.important;
+      return state.map((note) =>
+        note.id === changedNote.id ? changedNote : note
+      );
+    }
+    default:
+      return state;
   }
+};
 
-  export default noteReducer
+const createNote = (newNote) => {
+  return {
+    type: "NEW_NOTE",
+    payload: newNote,
+  };
+};
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: id,
+  };
+};
+
+export { createNote, toggleImportanceOf };
+
+export default noteReducer;
