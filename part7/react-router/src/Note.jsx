fceb18eacab9f6663ad78e6806c6f05ce32b1f31
Liza Maharjan <leeza.mhr@gmail.com>
2023-09-15 15:09:50 +0545

    react-router complete

---
+++ part7/react-router/src/Note.jsx
@@ -0,0 +1,13 @@
+const Note = ({ note }) => {
+
+  return (
+    <>
+      <h2>This is single note for {note.id}</h2>
+
+      <li>
+        {note.content} <strong>{note.important ? "important" : ""}</strong>
+      </li>
+    </>
+  );
+};
+export default Note;
