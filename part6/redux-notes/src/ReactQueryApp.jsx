2ae9bedacc873faed350c37f91a03adf19b91c39
Liza Maharjan <leeza.mhr@gmail.com>
2023-09-18 10:59:28 +0545

    complete

--- part6/redux-notes/src/ReactQueryApp.jsx
+++ part6/redux-notes/src/ReactQueryApp.jsx
@@ -1,61 +1,60 @@
-import { useQuery, useMutation} from '@tanstack/react-query'
-import { getNotes, createNote, updateNote } from './requests'
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getNotes, createNote, updateNote } from "./requests";
 
 const App = () => {
-    const newNoteMutation = useMutation(createNote, {
-        onSuccess: () => {
-          queryClient.invalidateQueries({ queryKey: ['notes'] })
-        },
-      }) 
-      
-      const updateNoteMutation = useMutation(updateNote, {
-        onSuccess: () => {
-          queryClient.invalidateQueries('notes')
-        },
-      })
+  const queryClient = useQueryClient();
 
-    const addNote = async (event) => {
-      event.preventDefault()
-      const content = event.target.note.value
-      newNoteMutation.mutate({ content, important: true })
-      event.target.note.value = ''
-      console.log(content)
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: getNotes,
+    refetchOnWindowFocus: false,
+  });
 
-    }
-  
-    const toggleImportance = (note) => {
-      console.log('toggle importance of', note.id)
-      updateNoteMutation.mutate(note.id)
-    }
+  const newNoteMutation = useMutation(createNote, {
+    onSuccess: (myNote) => {
+      const notes = queryClient.getQueryData(["notes"]);
+      queryClient.setQueryData(["notes"], notes.concat(myNote));
+    },
+  });
 
-    const result = useQuery({
-        queryKey: ['notes'],
-        queryFn: getNotes,
-      })
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
 
-    //   console.log(JSON.parse(JSON.stringify(result)))
-    
-      if ( result.isLoading ) {
-        return <div>loading data...</div>
-      }
-      
-      const notes = result.data
-  
-    return(
-      <div>
-        <h2>Notes app</h2>
-        <form onSubmit={addNote}>
-          <input name="note" />
-          <button type="submit">add</button>
-        </form>
-        {notes.map(note =>
-          <li key={note.id} onClick={() => toggleImportance(note)}>
-            {note.content} 
-            <strong> {note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </div>
-    )
+  const addNote = async (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    newNoteMutation.mutate({ content, important: true });
+    event.target.note.value = "";
+    // console.log(content);
+  };
+
+  const toggleImportance = (note) => {
+    console.log("toggle importance of", note.id);
+    updateNoteMutation.mutate({ ...note, important: !note.important });
+  };
+
+  //   console.log(JSON.parse(JSON.stringify(result)));
+  if (result.isLoading) {
+    return <div>loading data...</div>;
   }
-  
-  export default App
+  const notes = result.data;
+  return (
+    <div>
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map((note) => (
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
+        </li>
+      ))}
+    </div>
+  );
+};
+export default App;
