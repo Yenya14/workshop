3e02d94d4f1628f1cb6f77f017c1a44ef01ceb58
Liza Maharjan <leeza.mhr@gmail.com>
2023-09-29 15:46:36 +0545

    part 13 c updated

--- part13/notes-server/util/db.js
+++ part13/notes-server/util/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { DB_CONNECTION } = require("./config");
+const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize(DB_CONNECTION, {
   dialectOptions: {
@@ -7,19 +8,34 @@ const sequelize = new Sequelize(DB_CONNECTION, {
       require: true,
       rejectUnauthorized: false,
     },
-  },
+  }, 
 });
+
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.js",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
 
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("connected to the database");
   } catch (err) {
     console.log("failed to connect to the database");
     return process.exit(1);
   }
-
   return null;
 };
-
 module.exports = { sequelize, connectToDatabase };
