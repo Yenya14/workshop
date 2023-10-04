ca5a5e9473b6c1aab0765fdf7c40773054671701
Liza Maharjan <leeza.mhr@gmail.com>
2023-10-04 14:25:21 +0545

    part 9 complete

--- part9/multiplier.ts
+++ part9/multiplier.ts
@@ -1,7 +1,11 @@
-type Operation = "multiply" | "add" | "divide";
+export type Operation = "multiply" | "add" | "divide";
 type Result = number | string;
 
-const multiplicator = (a: number, b: number, operation: Operation): Result => {
+export const multiplicator = (
+  a: number,
+  b: number,
+  operation: Operation
+): Result => {
   if (operation === "divide") {
     if (b === 0) {
       throw new Error("Can't divide by 0!");
@@ -12,7 +16,6 @@ const multiplicator = (a: number, b: number, operation: Operation): Result => {
   }
   return "not valid operation";
 };
-
 try {
   console.log(multiplicator(2, 0, "divide"));
 } catch (e: unknown) {
